744aa9fadcc408dde1917cdc96f75d89ef608270
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2023-07-04 22:41:13 +0700

    Full ganti

--- index.php
+++ index.php
@@ -1,151 +1,92 @@
+<?php
+include 'koneksi.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://kit.fontawesome.com/85550cfb5f.js" crossorigin="anonymous"></script>
-    <title>Beranda</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&display=swap');
 
         * {
-            font-family: Poppins;
+            font-family: 'poppins';
         }
     </style>
 </head>
 
 <body>
-
-    <!-- Navbar Section -->
-    <nav class="relative px-4 py-4 flex justify-between items-center bg-white">
-        <a class="text-3xl font-bold leading-none" href="#">
-            RefreshJuice
-        </a>
-        <div class="lg:hidden">
-            <button class="navbar-burger flex items-center text-blue-600 p-3">
-                <svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <title>Mobile menu</title>
-                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-                </svg>
-            </button>
-        </div>
-        <ul class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
-            <li>
-                <a class="text-sm text-gray-400 hover:text-gray-500" href="#">Category</a>
-            </li>
-            <li>
-                <a class="text-sm text-blue-600 font-bold" href="#">Subscribe</a>
-            </li>
-            <li>
-                <a class="text-sm text-gray-400 hover:text-gray-500" href="#">Delivery</a>
-            </li>
-            <li>
-                <div class="spacer w-[200px]"></div>
-            </li>
-            <li>
-                <div class="search flex rounded w-[100%] h-8 overflow-hidden bg-gray-200">
-                    <input type="search" name="search" id="cari" placeholder="Search" class="outline-none px-3 w-full bg-transparent">
-                </div>
-            </li>
-        </ul>
-        <a class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 text-sm text-gray-900 font-bold" href="#">Login</a>
-    </nav>
-    <div class="navbar-menu relative z-50 hidden">
-        <div class="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
-        <nav class="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
-            <div class="flex items-center mb-8">
-                <a class="mr-auto text-3xl font-bold leading-none" href="#">
-                    RefreshJuice
-                </a>
-                <button class="navbar-close">
-                    <svg class="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                    </svg>
-                </button>
+    <div class="container mx-auto min-w-[40%] text-xl">
+        <!-- Navbar -->
+        <nav class="w-full flex justify-between items-center h-28 ">
+            <div class="logo flex space-x-2 items-center">
+                <img src="assets/logo/logo.png" class="w-[60px] h-[60px]" alt="Belum ada">
+                <p class="text-2xl font-bold">Refresh Juice</p>
             </div>
-            <div>
-                <ul>
-                    <li class="mb-1">
-                        <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Category</a>
-                    </li>
-                    <li class="mb-1">
-                        <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Subscribe</a>
-                    </li>
-                    <li class="mb-1">
-                        <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Delivery</a>
-                    </li>
-                </ul>
-                <div class="search flex rounded w-full bg-gray-300 h-10 overflow-hidden border">
-                    <input type="search" name="search" id="cari" class="outline-none w-full px-2" placeholder="Search">
-                </div>
-            </div>
-            <div class="mt-auto">
-                <div class="pt-6">
-                    <a class="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold leading-none bg-gray-50 hover:bg-gray-100 rounded-xl" href="#">Login</a>
-                    <a class="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-blue-600 hover:bg-blue-700  rounded-xl" href="#">Register</a>
-                </div>
-                <p class="my-4 text-xs text-gray-400">
-                    <span>Copyright © ZigZag</span>
-                </p>
+            <ul class="flex space-x-10">
+                <li><a class="hover:text-orange-600 font-bold" href="#">Beranda</a></li>
+                <li><a class="hover:text-orange-600" href="#">Kategori</a></li>
+                <li><a class="hover:text-orange-600" href="#">Langganan</a></li>
+                <li><a class="hover:text-orange-600" href="#">Pengiriman</a></li>
+            </ul>
+            <div class="flex space-x-7 text-orange-400">
+                <a href="#"><i class="fa-solid fa-magnifying-glass hover:text-orange-600"></i></a>
+                <a href="#"><i class="fa-solid fa-cart-shopping hover:text-orange-600"></i></a>
+                <a href="#"><i class="fa-solid fa-user hover:text-orange-600"></i></a>
+                
+                
+                
             </div>
         </nav>
+        <!-- End Navbar -->
+
+        <!-- Konten Produk -->
+        <?php
+
+        if (isset($_GET['cari'])) { // Jika pengguna melakukan pencarian
+            $key = $_GET['cari'];
+            $sql = "SELECT * FROM produk WHERE nama_pro = '$key'";
+            $query = mysqli_query($koneksi, $sql);
+
+            if ($query->num_rows > 0) { //Jika produk yang dicari ditemukan
+        ?>
+
+                <div class="grid grid-cols-4 gap-5">
+
+                </div>
+
+            <?php
+            } else { //Jika produk yang dicari tidak ditemukan
+            ?>
+
+
+
+                <?php
+            }
+        } else { // Jika pengguna tidak melakukan pencarian
+            $sql = "SELECT * FROM produk";
+            $query = mysqli_query($koneksi, $sql);
+
+            if ($query->num_rows > 0) {
+                while ($row = mysqli_fetch_array($query)) {
+                ?>
+
+                    <div class="grid grid-cols-4 gap-5">
+                        <div class="card-produk w-"></div>
+                    </div>
+
+        <?php
+                }
+            }
+        }
+
+        ?>
+        <!-- End Konten Produk -->
     </div>
-
-    <!-- Hero Section -->
-    
-    <section class="hero">
-        
-    </section>
-
-    <section class="kategori w-full">
-
-    </section>
 </body>
-
-
-<script>
-    // Burger menus
-    document.addEventListener('DOMContentLoaded', function() {
-        // open
-        const burger = document.querySelectorAll('.navbar-burger');
-        const menu = document.querySelectorAll('.navbar-menu');
-
-        if (burger.length && menu.length) {
-            for (var i = 0; i < burger.length; i++) {
-                burger[i].addEventListener('click', function() {
-                    for (var j = 0; j < menu.length; j++) {
-                        menu[j].classList.toggle('hidden');
-                    }
-                });
-            }
-        }
-
-        // close
-        const close = document.querySelectorAll('.navbar-close');
-        const backdrop = document.querySelectorAll('.navbar-backdrop');
-
-        if (close.length) {
-            for (var i = 0; i < close.length; i++) {
-                close[i].addEventListener('click', function() {
-                    for (var j = 0; j < menu.length; j++) {
-                        menu[j].classList.toggle('hidden');
-                    }
-                });
-            }
-        }
-
-        if (backdrop.length) {
-            for (var i = 0; i < backdrop.length; i++) {
-                backdrop[i].addEventListener('click', function() {
-                    for (var j = 0; j < menu.length; j++) {
-                        menu[j].classList.toggle('hidden');
-                    }
-                });
-            }
-        }
-    });
-</script>
 
 </html>
